b5108c655c377abcda9c5f6f147fcf547a981631
Zaman-shaikh <zamanshaikh24303@gmail.com>
2024-06-14 22:09:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("List-container");
+
+function addTask() {
+  if (inputBox.value === '') {
+    alert("You must write something!");
+  } else {
+    let li = document.createElement("li");
+    li.innerHTML = inputBox.value;
+    listContainer.appendChild(li);
+    let span = document.createElement("span");
+    span.innerHTML = "\u00d7";
+    li.appendChild(span);
+
+    inputBox.value = "";
+    saveData();
+  }
+}
+
+// Add event listener for both "click" on the button and "keypress" on the input field
+inputBox.addEventListener("keypress", function(event) {
+  if (event.key === "Enter") {
+    addTask();
+  }
+});
+
+listContainer.addEventListener("click", function(e) {
+  if (e.target.tagName === "LI") {
+    e.target.classList.toggle("checked");
+    saveData();
+  } else if (e.target.tagName === "SPAN") {
+    e.target.parentElement.remove();
+    saveData();
+  }
+}, false);
+
+function saveData() {
+  localStorage.setItem("data", listContainer.innerHTML);
+}
+
+function showTask() {
+  listContainer.innerHTML = localStorage.getItem("data");
+}
+
+showTask();
